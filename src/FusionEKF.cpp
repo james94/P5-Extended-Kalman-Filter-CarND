88c49082bbc3c7719cc55a5a50e4aa042e66ffb9
james94 <11840449+james94@users.noreply.github.com>
2019-11-21 18:44:41 -0800

    Implemented Sensor Fusion Flow
    1. Initialized variables and matrices (x, P, F, H_laser,
    H_jacobian, R_laser, R_radar, Q) for FusionEKF constructor
    
    2. Initialized Kalman filter position vector with the
    first sensor measurements for radar or lidar depending
    on sensor type (code update in ProcessMeasurement())
    
    3. Before EKF prediction, computed elapsed time dt between
    the current and previous observation. Modified the new
    state transition matrix F and process covariance matrix Q
    based on elapsed time (code added in ProcessMeasurement())
    
    4. Used sensor type (radar or lidar) to perform update
    step. If radar, computed jacobian Hj to linearize
    measurement function, set up radar measurement covariance
    matrix R_radar and called EKF update function. If lidar,
    set up lidar measurement and covariance matrix, then
    called KF update function.
    
    Verified code can compile successfully.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -14,30 +14,50 @@ using std::vector;
  */
 FusionEKF::FusionEKF() {
   is_initialized_ = false;
-
   previous_timestamp_ = 0;
 
-  // initializing matrices
-  R_laser_ = MatrixXd(2, 2);
-  R_radar_ = MatrixXd(3, 3);
-  H_laser_ = MatrixXd(2, 4);
-  Hj_ = MatrixXd(3, 4);
-
-  //measurement covariance matrix - laser
+  // Laser measurement covariance matrix R_laser
+  R_laser_ = MatrixXd(2, 2); 
   R_laser_ << 0.0225, 0,
               0, 0.0225;
 
-  //measurement covariance matrix - radar
+  // Radar measurement covariance matrix R_radar
+  R_radar_ = MatrixXd(3, 3); 
   R_radar_ << 0.09, 0, 0,
               0, 0.0009, 0,
               0, 0, 0.09;
 
-  /**
-   * TODO: Finish initializing the FusionEKF.
-   * TODO: Set the process and measurement noises
-   */
+  // Laser measurement matrix
+  H_laser_ = MatrixXd(2, 4); 
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
 
+  // Radar jacobian measurement matrix Hj_
+  Hj_ = MatrixXd(3, 4);
 
+  // 4D state vector
+  ekf_.x_ = VectorXd(4); 
+
+  // State covariance matrix
+  ekf_.P_ = MatrixXd(4, 4); 
+  ekf_.P_ << 1, 0, 0, 0,
+             0, 1, 0, 0,
+             0, 0, 1000, 0,
+             0, 0, 0, 1000;
+
+  // Initial state transition matrix
+  ekf_.F_ = MatrixXd(4, 4); 
+  ekf_.F_ << 1, 0, 1, 0,
+             0, 1, 0, 1,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
+
+  // Process noise covariance matrix
+  ekf_.Q_ = MatrixXd(4, 4); 
+
+  // Set process noise components ax, ay for process covariance matrix Q
+  noise_ax = 9;
+  noise_ay = 9;
 }
 
 /**
@@ -45,33 +65,50 @@ FusionEKF::FusionEKF() {
  */
 FusionEKF::~FusionEKF() {}
 
+/**
+ * ProcessMeasurement() function
+ * 
+ * This function is the "Sensor Fusion General Flow":
+ * 
+ * This function is triggered each time it receives new measurements from a
+ * lidar or radar sensor. At first iteration, the Kalman Filter position
+ * vector x is initialized with the first sensor measurements. Subsequently,
+ * the prediction and measurement update is called. Before prediction, the
+ * elapsed time is computed between the current and previous observation.
+ * Then based based on elapsed time, the new state transition and process
+ * covariance matrices are calculated. The measurement update depends on
+ * sensor type. If the current observation comes from a radar sensor, we
+ * compute the new Jacobian Hj matrix, use the nonlinear measurement function
+ * (ro, theta, ro_dot) to project the predicted state and call measurement
+ * UpdateEKF(). Otherwise, if the current observation comes from a lidar sensor
+ * , then we just set up the extended kalman filter with the laser H and R
+ * matrices and call measurement Update(). Both update functions update
+ * the state and covariance matrices.
+ */
 void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   /**
    * Initialization
    */
   if (!is_initialized_) {
-    /**
-     * TODO: Initialize the state ekf_.x_ with the first measurement.
-     * TODO: Create the covariance matrix.
-     * You'll need to convert radar from polar to cartesian coordinates.
-     */
-
-    // first measurement
-    cout << "EKF: " << endl;
-    ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
+    // Initialize Kalman Filter position vector with first sensor measurements
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-      // TODO: Convert radar from polar to cartesian coordinates 
-      //         and initialize state.
-
+      // Intialize state ekf_.x_ with first radar measurement (ro, theta, ro_dot)
+      ekf_.x_ << measurement_pack.raw_measurements_[0], // ro
+                 measurement_pack.raw_measurements_[1], // theta
+                 measurement_pack.raw_measurements_[2], // ro_dot
+                 0;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
-      // TODO: Initialize state.
-
+      // Intialize state ekf_.x_ with first laser measurement (px, py)
+      ekf_.x_ << measurement_pack.raw_measurements_[0], // px
+                 measurement_pack.raw_measurements_[1], // py
+                 0,
+                 0;
     }
 
-    // done initializing, no need to predict or update
+    previous_timestamp_ = measurement_pack.timestamp_;
+    // Done initializing, no need to predict or update
     is_initialized_ = true;
     return;
   }
@@ -80,12 +117,24 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    * Prediction
    */
 
-  /**
-   * TODO: Update the state transition matrix F according to the new elapsed time.
-   * Time is measured in seconds.
-   * TODO: Update the process noise covariance matrix.
-   * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
-   */
+  // Before Prediction (delta t (dt) - expressed in seconds):
+  // Compute the time elapsed between the current and previous measurements
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  float dt_2 = dt*dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+
+  // Update the state transition matrix F according to the new elapsed time
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+
+  // Update the process noise covariance matrix Q based on elapsed time
+  ekf_.Q_ << (dt_4/4)*noise_ax, 0, (dt_3/2)*noise_ax, 0,
+             0, (dt_4/4)*noise_ay, 0, (dt_3/2)*noise_ay,
+             (dt_3/2)*noise_ax, 0, dt_2*noise_ax, 0,
+             0, (dt_3/2)*noise_ay, 0, dt_2*noise_ay;
 
   ekf_.Predict();
 
@@ -93,21 +142,38 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    * Update
    */
 
-  /**
-   * TODO:
-   * - Use the sensor type to perform the update step.
-   * - Update the state and covariance matrices.
-   */
-
+  // Use the sensor type (radar or lidar) to perform the update step
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-    // TODO: Radar updates
+    // Set up kalman filter with radar Hj and R matrices
+
+    // Compute the new jacobian Hj to linearize measurement function h(x')
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = MatrixXd(3, 4);
+    ekf_.H_ = Hj_;
+
+    // Set up radar measurement covariance matrix
+    ekf_.R_ = MatrixXd(3, 3);
+    ekf_.R_ = R_radar_;
+
+    // Call measurement UpdateEKF with radar data to update state and covariance
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
 
   } else {
-    // TODO: Laser updates
+    // Set up kalman filter with laser H and R matrices
 
+    // Set up lidar measurement matrix
+    ekf_.H_ = MatrixXd(2, 4); 
+    ekf_.H_ = H_laser_;
+
+    // Set up lidar measurement covariance matrix
+    ekf_.R_ = MatrixXd(2, 2);
+    ekf_.R_ = R_laser_;
+
+    // Call measurement Update with lidar data to update state and covariance
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
-  // print the output
+  // print the updated state and covariance matrices
   cout << "x_ = " << ekf_.x_ << endl;
   cout << "P_ = " << ekf_.P_ << endl;
 }
