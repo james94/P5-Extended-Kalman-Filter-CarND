507ece68867002f8cc3b83e2d8a4f7c56e1df96b
james94 <11840449+james94@users.noreply.github.com>
2019-11-22 18:39:53 -0800

    Implemented code for prediction and update steps
    The prediction step computes a prediction for x state
    and P state covariance. The standard Kalman Filter
    Update step updates the lidar measurement state and
    covariance. The Extended Kalman Filter UpdateEKF step
    updates the radar measurement state and covariance.
    The UpdateEKF does extra processing on predicted x
    state to convert cartesian back to polar system to
    compare radar predicted x state against new state
    and retrieve y error. There is a UpdateWithY
    function that finishes updating the sensor
    measurement based on the y error passed into it.
    There is a y error for lidar and radar. Tested and
    verified simulator and C++ program work with this
    code. rmse also meets Udacity rubric specification
    for this project.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,7 +1,10 @@
 #include "kalman_filter.h"
+#include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using std::cout;
+using std::endl;
 
 /* 
  * Please note that the Eigen library does not initialize 
@@ -13,7 +16,8 @@ KalmanFilter::KalmanFilter() {}
 KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in)
+{
   x_ = x_in;
   P_ = P_in;
   F_ = F_in;
@@ -22,20 +26,77 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
   Q_ = Q_in;
 }
 
-void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+/**
+* predict the x state and P state covariance
+*/
+void KalmanFilter::Predict()
+{
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
-void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+/**
+* Update the measurement of lidar x state 
+* and P state covariance by using Kalman 
+* Filter equations
+*/
+void KalmanFilter::Update(const VectorXd &z)
+{
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+
+  UpdateWithY(y);
 }
 
-void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+/**
+* Update the measurement of radar x state 
+* and P state covariance by using Extended 
+* Kalman Filter equations
+*/
+void KalmanFilter::UpdateEKF(const VectorXd &z)
+{
+  // Recover state parameters
+  double px = x_(0);
+  double py = x_(1);
+  double vx = x_(2);
+  double vy = x_(3);
+
+  double rho = sqrt(px * px + py * py);
+  double phi = atan2(py, px);
+  double rho_dot = (px * vx + py * vy) / (rho);
+
+  VectorXd hx_func(3);
+  hx_func << rho, phi, rho_dot;
+
+  // compute state error between actual and predicted state 
+  VectorXd y = z - hx_func;
+
+  // Normalize angle in y vector to be between -PI and +PI
+  while (y(1) > M_PI)
+  {
+    y(1) -= 2 * M_PI;
+  }
+
+  while (y(1) < -M_PI)
+  {
+    y(1) += 2 * M_PI;
+  }
+
+  UpdateWithY(y);
+}
+
+void KalmanFilter::UpdateWithY(const VectorXd &y)
+{
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  // new estimate x state vector and P covariance matrix
+  x_ = x_ + (K * y);
+  int x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
