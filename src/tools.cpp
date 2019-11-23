048af13bf19919418371c08ad9042a521d528c30
james94 <11840449+james94@users.noreply.github.com>
2019-11-22 17:39:59 -0800

    Tested simulator after reformatting code
    Reformatted the code. Launched simulator to test and verify rmse
    was being updated live to the simulator and met udacity rubric
    specifications

--- src/tools.cpp
+++ src/tools.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <cmath>
 
-using Eigen::VectorXd;
 using Eigen::MatrixXd;
-using std::vector;
+using Eigen::VectorXd;
 using std::cout;
 using std::endl;
+using std::vector;
 
 Tools::Tools() {}
 
@@ -20,40 +20,40 @@ Tools::~Tools() {}
 * computes the mean and square root of mean to calculate the RMSE
 */
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  // Initialize rmse
-  VectorXd rmse(4);
-  rmse << 0, 0, 0, 0;
+                              const vector<VectorXd> &ground_truth)
+{
+   // Initialize rmse
+   VectorXd rmse(4);
+   rmse << 0, 0, 0, 0;
 
-  // Initialize number of elements in estimation vector
-  int estimations_size = estimations.size();
+   // Initialize number of elements in estimation vector
+   int estimations_size = estimations.size();
 
-  // check the validity for inputs:
-  // the estimation vector size should not be zero
-  // the estimation vector size should equal ground truth vector size
-  if(estimations_size == 0 || (estimations_size != ground_truth.size()))
-  {
-     cout << "Invalid estimation or ground truth data" << endl;
-  }
+   // check the validity for inputs:
+   // the estimation vector size should not be zero
+   // the estimation vector size should equal ground truth vector size
+   if (estimations_size == 0 || (estimations_size != ground_truth.size()))
+   {
+      cout << "Invalid estimation or ground truth data" << endl;
+   }
+   else
+   {
+      // accumulate squared residuals
+      for (unsigned int i = 0; i < estimations_size; ++i)
+      {
+         VectorXd residual = estimations[i] - ground_truth[i];
+         residual = residual.array() * residual.array();
+         rmse += residual; // compute residual sum
+      }
 
-  else
-  {
-     // accumulate squared residuals
-     for(int i = 0; i < estimations_size; ++i)
-     {
-        VectorXd residual = estimations[i] - ground_truth[i];
-        residual = residual.array() * residual.array();
-        rmse += residual; // compute residual sum
-     }
+      // calculate the residua mean
+      rmse = rmse / estimations_size;
+      // calculate the squared root of mean
+      rmse = rmse.array().sqrt();
+   }
 
-     // calculate the residua mean
-     rmse = rmse/estimations_size;
-     // calculate the squared root of mean
-     rmse = rmse.array().sqrt();
-  }
-  
-  // return the result
-  return rmse;
+   // return the result
+   return rmse;
 }
 
 /**
@@ -63,32 +63,33 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 * of terms to avoid repeated calculation, checks division by
 * zero and computes jacobian matrix
 */
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  // Initialize Jacobian Matrix
-  MatrixXd Hj(3, 4);
+MatrixXd Tools::CalculateJacobian(const VectorXd &x_state)
+{
+   // Initialize Jacobian Matrix
+   MatrixXd Hj(3, 4);
 
-  // Recover state parameters
-  float px = x_state(0);
-  float py = x_state(1);
-  float vx = x_state(2);
-  float vy = x_state(3);
+   // Recover state parameters
+   double px = x_state(0);
+   double py = x_state(1);
+   double vx = x_state(2);
+   double vy = x_state(3);
 
-  // Pre-compute a set of terms to avoid repeated calculation
-  float c1 = pow(px, 2) + pow(py, 2);
-  float c2 = sqrt(c1);
-  float c3 = (c1 * c2);
+   // Pre-compute a set of terms to avoid repeated calculation
+   double c1 = pow(px, 2) + pow(py, 2);
+   double c2 = sqrt(c1);
+   double c3 = (c1 * c2);
 
-  // Check division by zero: x nor y should be 0
-  if(px == 0 && py == 0)
-  {
-     cout << "CalculationJacobian () - Error - Division by Zero" << endl;
-  }
-  // Compute the Jacobian matrix
-  else
-  {
-     Hj << (px/c2), (py/c2), 0, 0,
-           -(py/c1), (px/c1), 0, 0,
-           py*(vx*py - vy*px)/c3, px*(vy*px - vx*py)/c3, (px/c2), (py/c2);
-  }
-  return Hj;
+   // Check division by zero: x nor y should be 0
+   if (px == 0 && py == 0)
+   {
+      cout << "CalculationJacobian () - Error - Division by Zero" << endl;
+   }
+   // Compute the Jacobian matrix
+   else
+   {
+      Hj << (px / c2), (py / c2), 0, 0,
+          -(py / c1), (px / c1), 0, 0,
+          py * (vx * py - vy * px) / c3, px * (vy * px - vx * py) / c3, (px / c2), (py / c2);
+   }
+   return Hj;
 }
